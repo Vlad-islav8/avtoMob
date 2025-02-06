3a8bf4a1d465b2f79fcfe67c6b400b85a6689719
Vlad islav <vladfedevg@gmail.com>
2025-02-06 17:20:11 +0500

    1.0.1

--- src/components/Home/HereSection/HeroSection.jsx
+++ src/components/Home/HereSection/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './HeroSection.module.css';
 import Cont from '../../Container/Container.module.css';
+import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
     return (
@@ -9,7 +10,7 @@ const HeroSection = () => {
                 <div className={styles.heroContent}>
                     <h1>avtoMob</h1>
                     <p>Автомобили с пробегом по самым приятным ценам</p>
-                    <a href="#" className={styles.ctaButton}>Узнать больше</a>
+                    <Link to="/search" className={styles.ctaButton}>Подобрать авто</Link>
                 </div>
             </div>
         </section>
